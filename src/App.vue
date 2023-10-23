728e0f66882be844174e3f9fa06520d9c88c2e91
hujinbin <437044048@qq.com>
2023-10-23 10:52:05 +0800

    feat: 数据懒加载demo

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="ve-tree">
     <div class="ve-tree-flex">
+    <h4>数据全量加载</h4>
     <big-data-tree
       node-key="id"
       :data="treeData"
       :props="props"
       :item-size="26"
-      height="calc(100vh - 30px)"
+      height="calc(100vh - 100px)"
       show-checkbox
     >
     </big-data-tree>
   </div>
   <div class="ve-tree-flex">
+    <h4>数据分段加载</h4>
     <big-data-tree
       node-key="id"
       :props="props"
       :item-size="26"
       :load="loadNode"
       lazy
-      height="calc(100vh - 30px)"
+      height="calc(100vh - 100px)"
       show-checkbox
     >
     </big-data-tree>
@@ -33,9 +35,11 @@ export default {
       props: {
         label: "name",
         children: "children",
+        isLeaf: 'isLeaf'
       },
       treeData: [],
       lazyTreeData:[],
+      treeMap:{},
     };
   },
   created() {
@@ -57,6 +61,7 @@ export default {
         total: children,
         children: [],
       })
+      let level2 = []
       for (let j = 0; j < children; j++) {
         data[i].children.push({
           id: `${i}-${j}`,
@@ -64,14 +69,29 @@ export default {
           total: base,
           children: [],
         });
+        level2.push({
+          id: `${i}-${j}`,
+          name: `test-${i}-${j}`,
+          total: base,
+          children: [],
+        })
+        let level3 = []
         for (let k = 0; k < base; k++) {
           data[i].children[j].children.push({
             id: `${i}-${j}-${k}`,
             name: `test-${i}-${j}-${k}`,
             total: 0,
           });
+          level3.push({
+            id: `${i}-${j}-${k}`,
+            name: `test-${i}-${j}-${k}`,
+            total: 0,
+            isLeaf: true,
+          })
         }
+        this.treeMap[`${i}-${j}`] = level3
       }
+      this.treeMap[`${i}`] = level2
     }
     console.log(lazyData)
     this.treeData = data;
@@ -84,10 +104,9 @@ export default {
         if (node.level === 0) {
           return resolve(this.lazyTreeData);
         }
-        if (node.level > 1) return resolve([]);
-
+        if (node.level > 2) return resolve([]);
         setTimeout(() => {
-          const children = this.treeData.find(item => item.id === node.data.id).children;
+          const children = this.treeMap[node.data.id];
           resolve(children);
         }, 500);
      }
